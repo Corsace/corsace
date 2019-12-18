fbcee819739948d3d3e0b88e4677934c9d63b28a
VINXIS <oykxf2@gmail.com>
2019-12-18 02:43:00 -0700

    add hosts and ports to config

--- config.example.ts
+++ config.example.ts
@@ -2,27 +2,72 @@ export class Config {
     database : {
         name: string,
         username: string,
-        password: string
+        password: string,
     };
 
     osuApi : {
         id: number,
         redirect: string,
         secret: string,
-        v1: string
+        v1: string,
     };
+
+    ayim : {
+        host: string,
+        port: number,
+    }
+
+    corsace : {
+        host: string,
+        port: number,
+    }
+
+    invitational : {
+        host: string,
+        port: number,
+    }
+
+    mca : {
+        host: string,
+        port: number,
+    }
+
+    open : {
+        host: string,
+        port: number,
+    }
 
     constructor() {
         this.database = {
             name: '',
             username: '',
-            password: ''
+            password: '',
         },
         this.osuApi = {
             id: 0,
             redirect: '',
             secret: '',
-            v1: ''
+            v1: '',
+        },
+        this.ayim = {
+            host: '',
+            port: 0,
+        }
+        this.corsace = {
+            host: '',
+            port: 0,
+        }
+        this.invitational = {
+            host: '',
+            port: 0,
+        }
+        this.mca = {
+            host: '',
+            port: 0,
+        }
+        this.open = {
+            host: '',
+            port: 0,
         }
     }
 }
