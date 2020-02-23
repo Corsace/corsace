ac535f1163ccf8519d72bfa0795639120e04d047
VINXIS <oykxf2@gmail.com>
2020-02-23 07:47:00 -0700

    update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,44 +4,72 @@ module.exports = {
         browser: true,
         node: true
     },
-    parser: 'vue-eslint-parser',
+    parser: "vue-eslint-parser",
     parserOptions: {
-        parser: '@typescript-eslint/parser',
+        parser: "@typescript-eslint/parser",
         tsconfigRootDir: __dirname,
-        project: ['./tsconfig.json'],
-        extraFileExtensions: ['.vue']
+        project: ["./tsconfig.json"],
+        extraFileExtensions: [".vue"]
     },
     plugins: [
-        '@typescript-eslint',
-        'vue'
+        "@typescript-eslint",
+        "vue"
     ],
     extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:vue/recommended',
-        'prettier/@typescript-eslint'
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:vue/recommended",
+        "prettier/@typescript-eslint"
     ],
     rules: {
-        '@typescript-eslint/no-misused-promises': 'off',
-        '@typescript-eslint/ban-ts-ignore': 'off',
-        '@typescript-eslint/camelcase': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        'vue/html-indent': 'off'
+        "@typescript-eslint/no-misused-promises": "off",
+        "@typescript-eslint/ban-ts-ignore": "off",
+        "@typescript-eslint/camelcase": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "vue/html-indent": "off"
     },
     overrides: [
         {
-            'files': ['*.vue'],
-            'rules': {
-                '@typescript-eslint/explicit-function-return-type': 'off'
+            "files": ["*.vue"],
+            "rules": {
+                "@typescript-eslint/explicit-function-return-type": "off"
             }
         },
         {
-            'files': ['./CorsaceModels/**/*.ts'],
-            'rules': {
-                '@typescript-eslint/no-unused-vars': 'off'
+            "files": ["./CorsaceModels/**/*.ts"],
+            "rules": {
+                "@typescript-eslint/no-unused-vars": "off"
             }
         }
-    ]
+    ],
+    "vue/html-indent": [
+        "error", 
+        4
+    ],
+    semi: [
+        "error",
+        "always"
+    ],
+    quotes: [
+        "error",
+        "double",
+        { allowTemplateLiterals: true }
+    ],
+    indent: [
+        "error",
+        4,
+        { SwitchCase: 1 }
+    ],
+    "comma-dangle": [
+        "error", {
+            arrays: "always-multiline",
+            objects: "always-multiline",
+            imports: "always-multiline",
+            exports: "always-multiline",
+            functions: "always-multiline",
+            functions: "never",
+        },
+    ],
 };
