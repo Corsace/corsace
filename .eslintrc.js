7fa112d317cfd79eb6cf723f1e5dd2ff119aa504
VINXIS <oykxf2@gmail.com>
2020-02-23 11:13:05 -0700

    wsoops

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,34 @@ module.exports = {
         "@typescript-eslint/ban-ts-ignore": "off",
         "@typescript-eslint/camelcase": "off",
         "@typescript-eslint/no-explicit-any": "off",
-        "vue/html-indent": "off"
+        "vue/html-indent": [
+            "error", 
+            4
+        ],
+        semi: [
+            "error",
+            "always"
+        ],
+        quotes: [
+            "error",
+            "double",
+            { allowTemplateLiterals: true }
+        ],
+        indent: [
+            "error",
+            4,
+            { SwitchCase: 1 }
+        ],
+        "comma-dangle": [
+            "error", {
+                arrays: "always-multiline",
+                objects: "always-multiline",
+                imports: "always-multiline",
+                exports: "always-multiline",
+                functions: "always-multiline",
+                functions: "never",
+            },
+        ],
     },
     overrides: [
         {
@@ -43,33 +70,5 @@ module.exports = {
                 "@typescript-eslint/no-unused-vars": "off"
             }
         }
-    ],
-    "vue/html-indent": [
-        "error", 
-        4
-    ],
-    semi: [
-        "error",
-        "always"
-    ],
-    quotes: [
-        "error",
-        "double",
-        { allowTemplateLiterals: true }
-    ],
-    indent: [
-        "error",
-        4,
-        { SwitchCase: 1 }
-    ],
-    "comma-dangle": [
-        "error", {
-            arrays: "always-multiline",
-            objects: "always-multiline",
-            imports: "always-multiline",
-            exports: "always-multiline",
-            functions: "always-multiline",
-            functions: "never",
-        },
-    ],
+    ]
 };
