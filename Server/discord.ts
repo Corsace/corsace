582125eca6972e733a9fd0f8e91716ee804e9929
Roan Song <roansong@gmail.com>
2021-03-21 20:04:51 +0200

    Stop Discord logging in every time module is imported (e.g. in build) (#20)
    * Changed how logins to Discord bot function
    
    * add dist folders to gitignore
    
    Co-authored-by: VINXIS <oykxf2@gmail.com>

--- Server/discord.ts
+++ Server/discord.ts
@@ -1,16 +1,27 @@
 import * as Discord from "discord.js";
-import { config } from "node-config-ts";
+import {config} from "node-config-ts";
 
-const discordClient = new Discord.Client;
+let loggedIn = false;
+const _discordClient = new Discord.Client;
+const discordClient = (): Discord.Client => {
+    if (!loggedIn) {
+        loggedIn = true;
+        _discordClient.login(config.discord.token).then(() => {
+            console.log("Logged into discord!");
+        }).catch(err => {
+            if (err) {
+                loggedIn = false;
+                throw err;
+            }
+        });
+    }
+    return _discordClient;
+};
 
-discordClient.login(config.discord.token).then(() => {
-    console.log("Logged into discord!");
-}).catch(err => {if (err) throw err;});
-
-discordClient.on("error", err => {
+_discordClient.on("error", err => {
     console.log(err);
 });
 
-const discordGuild = (): Promise<Discord.Guild> => discordClient.guilds.fetch(config.discord.guild);
+const discordGuild = (): Promise<Discord.Guild> => discordClient().guilds.fetch(config.discord.guild);
 
-export { discordClient, discordGuild };
+export {discordClient, discordGuild};
