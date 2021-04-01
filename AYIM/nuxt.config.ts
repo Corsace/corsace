3299e2ed0593353c3266602e4703f4a8b3c5aefb
Hugo Denizart <thepoon@osufr.live>
2021-04-01 19:14:07 +0200

    Add SPA config entry and various fixes (#27)
    * 🔧 Fix config typings and axios config in builds
    
    * 🐛 Fix invalid dates in MCA creation
    
    * ✨ Allow enabling/disabling SPA at runtime
    
    We always buid with SSR enabled, but sometimes, we might want to run in SPA mode for performance reasons.
    
    * ♻️ Set axios config in base nuxt config
    
    * remove 2020
    
    Co-authored-by: VINXIS <oykxf2@gmail.com>

--- AYIM/nuxt.config.ts
+++ AYIM/nuxt.config.ts
@@ -1,32 +1,26 @@
-import { config } from "node-config-ts";
+import { NuxtConfig } from "@nuxt/types";
 import nuxtConfig from "../MCA-AYIM/nuxt.base.config";
 
 export default {
-    ...nuxtConfig,
-    ...{
-        server: {
-            host: config.ayim.host,
-            port: config.ayim.port,
-        },
-        head: {
-            link: [
-                { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap" },
-                { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Lexend+Peta&display=swap" },
-                { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-            ],
-            meta: [
-                { charset: "utf-8" },
-                { name: "viewport", content: "width=device-width, initial-scale=1" },
-                {
-                    hid: "description",
-                    name: "description",
-                    content: "AYIM 2020",
-                },
-            ],
-        },
-        css: [
-            "../MCA-AYIM/assets/sass/main.scss",
-            "./assets/ayim.scss",
+    ...nuxtConfig("ayim"),
+    head: {
+        link: [
+            { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap" },
+            { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Lexend+Peta&display=swap" },
+            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        ],
+        meta: [
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
+            {
+                hid: "description",
+                name: "description",
+                content: "AYIM",
+            },
         ],
     },
-};
+    css: [
+        "../MCA-AYIM/assets/sass/main.scss",
+        "./assets/ayim.scss",
+    ],
+} as NuxtConfig;
