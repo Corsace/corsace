72ccc98f8084dedae07d0fce2834a632f0e2f200
VINXIS <oykxf2@gmail.com>
2021-05-16 23:20:10 -0600

    index admin page on mca/ayim

---
+++ AYIM/pages/_year/admin/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <nuxt-link
+            :to="`/${$route.params.year}/admin/years`"
+            class="admin-page__link"
+        >
+            Years >
+        </nuxt-link>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+    head () {
+        return {
+            title: "Admin | MCA",
+        };
+    },
+})
+export default class Years extends Vue {
+    
+}
+</script>
