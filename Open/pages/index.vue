f5bc1c1fdfdd33cb2a393a393011e9d453d39687
VINXIS <oykxf2@gmail.com>
2021-06-04 23:29:23 -0600

    initial commit(?) for corsace open

---
+++ Open/pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>heyyyy lol</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import { State } from "vuex-class";
+
+import LoginModal from "../../MCA-AYIM/components/header/LoginModal.vue";
+
+import { UserInfo } from "../../Interfaces/user";
+
+@Component({
+    components: {
+        LoginModal,
+    },
+    head () {
+        return {
+            title: "Corsace Open",
+        };
+    },
+})
+export default class Default extends Vue {
+
+    @State loggedInUser!: UserInfo;
+
+    showLoginModal = false;
+
+    get avatarURL (): string  {
+        return this.loggedInUser?.osu?.avatar || "";
+    }
+    
+    toogleLoginModal (): void {
+        this.showLoginModal = !this.showLoginModal;
+    }
+
+    async mounted () {
+        await this.$store.dispatch("setInitialData");
+    }
+    
+}
+</script>
+
+<style lang="scss">
+
+</style>
