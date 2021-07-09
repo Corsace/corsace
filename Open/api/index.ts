01624fdfd5f1334a541d6bfbd22204fc82d0ca09
VINXIS <oykxf2@gmail.com>
2021-07-08 18:28:56 -0600

    add team router + remove formidable + middleware change

--- Open/api/index.ts
+++ Open/api/index.ts
@@ -3,11 +3,13 @@ import { App } from "../../Server";
 import mount from "koa-mount";
 import loginRouter from "../../Server/login"
 import userRouter from "./routes/user";
+import teamRouter from "./routes/team";
 
 const app = new App();
 
 app.koa.use(mount("/login", loginRouter.routes()));
 app.koa.use(mount("/user", userRouter.routes()));
+app.koa.use(mount("/team", teamRouter.routes()));
 
 export default {
     path: "/api",
