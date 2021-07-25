6fe9c6cced1cd1ceaedf31487feee29f95c1abf6
Kevin Huang <kevinhuang2k@g.ucla.edu>
2021-07-25 12:31:08 -0700

    ported stuff for poolers

--- Open/components/mappool/MapComponent.vue
+++ Open/components/mappool/MapComponent.vue
@@ -6,7 +6,7 @@
                     <div class="mapArtist">{{ map.artist }}</div>
                     <div class="mapTitle">{{ map.title }}</div>
                 </div>
-                <div class="inputWrapper" v-if="false && edit"> <!-- remember to check ifpooler -->
+                <div class="inputWrapper" v-if="loggedInUser.openStaff.isMappooler && edit">
                     <input v-model="mapURL" class="mapUrl" spellcheck="false" @input="changeMap">
                 </div>
                 <div class="mapRight">
@@ -33,7 +33,7 @@
                 <div class="mapArtist">Artist</div>
                 <div class="mapTitle">Title</div>
             </div>
-            <div class="inputWrapper" v-if="false && edit"> <!-- remember to check ifpooler -->
+            <div class="inputWrapper" v-if="loggedInUser.openStaff.isMappooler && edit">
                 <input v-model="mapNewURL" class="mapUrl" placeholder="paste url here (no /s/ links)" spellcheck="false" @input="addMap">
             </div>
             <div class="mapRight">
@@ -55,13 +55,11 @@
 </template>
 
 <script lang='ts'>
-import axios from "axios";
-import regeneratorRuntime from "regenerator-runtime";
 import { Component, Vue, Prop } from "vue-property-decorator"
 import { State } from "vuex-class"
 import { MappoolMap } from "../../../Interfaces/mappool";
 import { UserOpenInfo } from "../../../Interfaces/user";
-
+import axios from "axios"
 @Component
 export default class MapComponent extends Vue {
 
@@ -123,47 +121,46 @@ export default class MapComponent extends Vue {
             return;
         }
     }
-    /*
-    methods: {
-        addMap: async function() {
-            const result = this.regex.exec(this.mapNewURL);
+    //todo: test these
+    async addMap () {
+        const result = this.regex.exec(this.mapNewURL);
+        if(result) {
+            let id = "0";
+            if (result[1] === "b" || result[1] === "beatmaps")
+                id = result[2];
+            else if (result[1] === "beatmapsets")
+                id = result[4]
+            await axios.post("/api/mappool/add?map=" + id + "&round=" + this.round.replace(/ /g, "%20") + "&mod=" + this.mod);
+            this.mapURL = `https://osu.ppy.sh/b/${id}`;
+            this.$emit("refresh");
+        } else {
+            alert("Map does not exist/your link is bad!");
+            this.mapNewURL = "";
+        }
+    }
+
+    async changeMap () {
+        if(this.mapURL.length === 0) {
+            await axios.delete("/api/mappool/delete?map=" + this.map.mapID + "&round=" + this.round.replace(/ /g, "%20") + "&mod=" + this.mod);
+            this.mapNewURL = "";
+            this.$emit("refresh");
+        } else {
+            const result = this.regex.exec(this.mapURL);
             if(result) {
-                let id = 0;
+                let id = "0";
                 if (result[1] === "b" || result[1] === "beatmaps")
                     id = result[2];
                 else if (result[1] === "beatmapsets")
                     id = result[4]
-                await axios.post("/api/mappool/add?map=" + id + "&round=" + this.round.replace(/ /g, "%20") + "&mod=" + this.mod);
+                await axios.patch("/api/mappool/replace?oldMap=" + this.map.mapID + "&map=" + id + "&round=" + this.round.replace(/ /g, "%20") + "&mod=" + this.mod);
                 this.mapURL = `https://osu.ppy.sh/b/${id}`;
                 this.$emit("refresh");
             } else {
                 alert("Map does not exist/your link is bad!");
-                this.mapNewURL = "";
-            }
-        },
-        changeMap: async function() {
-            if(this.mapURL.length === 0) {
-                await axios.delete("/api/mappool/delete?map=" + this.map.mapID + "&round=" + this.round.replace(/ /g, "%20") + "&mod=" + this.mod);
-                this.mapNewURL = "";
-                this.$emit("refresh");
-            } else {
-                const result = this.regex.exec(this.mapURL);
-                if(result) {
-                    let id = 0;
-                    if (result[1] === "b" || result[1] === "beatmaps")
-                        id = result[2];
-                    else if (result[1] === "beatmapsets")
-                        id = result[4]
-                    await axios.patch("/api/mappool/replace?oldMap=" + this.map.mapID + "&map=" + id + "&round=" + this.round.replace(/ /g, "%20") + "&mod=" + this.mod);
-                    this.mapURL = `https://osu.ppy.sh/b/${id}`;
-                    this.$emit("refresh");
-                } else {
-                    alert("Map does not exist/your link is bad!");
-                    this.mapURL = `https://osu.ppy.sh/b/${this.map.mapID}`;
-                }
+                this.mapURL = `https://osu.ppy.sh/b/${this.map.mapID}`;
             }
         }
-    }*/
+    }
 }
 </script>
 
