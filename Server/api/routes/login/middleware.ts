a37be143f3460f54ba306e24e5e7e1b8e36fe48b
ThePooN <thepoon@osufr.live>
2022-02-22 19:09:56 +0100

    🐛 💩 Work-around authentication redirection issue in dev

--- Server/api/routes/login/middleware.ts
+++ Server/api/routes/login/middleware.ts
@@ -1,12 +1,12 @@
 import { config } from "node-config-ts";
 import { ParameterizedContext, Next } from "koa";
 
-const mainHost = new URL(config.corsace.publicUrl).host;
+const mainHost = new URL(config[process.env.NODE_ENV === "production" ? "corsace" : "api"].publicUrl).host;
 
 export async function redirectToMainDomain (ctx: ParameterizedContext, next: Next): Promise<void> {
     // Redirect to the main host (where user gets redirected post-oauth) to apply redirect cookie on the right domain
     if(ctx.host !== mainHost) {
-        ctx.redirect(`${config.corsace.publicUrl}${ctx.originalUrl}`);
+        ctx.redirect(`${config[process.env.NODE_ENV === "production" ? "corsace" : "api"].publicUrl}${ctx.originalUrl}`);
         return;
     }
     await next();
