ad29eb6877b82e4bb0d1e59deff3cdc704ac7c3a
ThePooN <thepoon@osufr.live>
2022-06-20 05:35:14 +0200

    ♻️ Refactor beatmap nominators migration

--- Models/migrations/1654558569754-BeatmapNominators.ts
+++ Models/migrations/1654558569754-BeatmapNominators.ts
@@ -1,84 +1,54 @@
-import { createReadStream } from "fs";
-import {MigrationInterface, QueryRunner} from "typeorm";
-import { createGunzip } from "zlib";
-import streamToString from "stream-to-string";
-import { resolve } from "path";
-import { OAuth, User } from "../user";
-
-const missingUser = {
-    username: "Enneya",
-    userID: 10959501,
-    country: "BE",
-    setIDs: [
-        1224699,
-        1061572,
-        1213339,
-        982104,
-        1250030,
-        1009447,
-        1261295,
-        1249344,
-        946155,
-        1190716,
-        1250546,
-        1155217,
-        1242733,
-        1118283,
-        1255210,
-        1233752,
-        1206933,
-        1233950,
-        1204933,
-        872150,
-        1293912,
-        973844,
-        1242515,
-        1178098,
-        1001825,
-        1317896,
-        1128882,
-        1254404,
-        1292185,
-        1153893
-    ]
-}
+import {FindCondition, MigrationInterface, MoreThan, QueryRunner} from "typeorm";
+import { Beatmapset } from "../beatmapset";
+import { config } from "node-config-ts";
+import { MoreThanOrEqual } from "typeorm";
+import { BNEvent, getBNsApiCallRawData, getRankers } from "../../Server/scripts/fetchYearMaps";
 
 export class BeatmapNominators1654558569754 implements MigrationInterface {
     name = 'BeatmapNominators1654558569754'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`user_maps_ranked_beatmapset\` (\`userID\` int NOT NULL, \`beatmapsetID\` int NOT NULL, INDEX \`IDX_3cda7daeb4ff2b0dbc4b90736c\` (\`userID\`), INDEX \`IDX_f0b456f4502aebcbfd9233edf7\` (\`beatmapsetID\`), PRIMARY KEY (\`userID\`, \`beatmapsetID\`)) ENGINE=InnoDB`);
-        const bigSql = await streamToString(createReadStream(resolve(__dirname, "1654558569754-BeatmapNominators.sql.gz")).pipe(createGunzip()));
-        const sqlInstructions = bigSql.split("\n").filter(sql => sql.trim().length !== 0);
-        for(const sqlInstruction of sqlInstructions)
-            if(sqlInstruction.trim().length !== 0)
-                await queryRunner.query(sqlInstruction);
-        
-        let user = await queryRunner.manager
-            .createQueryBuilder()
-            .from(User, "user")
-            .leftJoin("user.otherNames", "otherName")
-            .where("user.osuUserid = :userId", { userId: missingUser.userID })
-            .orWhere("user.osuUsername = :username")
-            .orWhere("otherName.name = :username")
-            .setParameter("username", missingUser.username)
-            .getOne();
-        if (!user) {
-            console.log("creating missing user", missingUser.userID, missingUser.username);
-    
-            user = new User();
-            user.osu = new OAuth();
-            user.osu.username = missingUser.username;
-            user.osu.userID = `${missingUser.userID}`;
-            user.country = missingUser.country;
-            await queryRunner.manager.save(user);
-        }
-        
-        for (const setID of missingUser.setIDs)
-            await queryRunner.query(`INSERT INTO \`user_maps_ranked_beatmapset\` (\`userID\`, \`beatmapsetID\`) VALUES (${user.ID},${setID})`);
 
         await queryRunner.query(`ALTER TABLE \`user_maps_ranked_beatmapset\` ADD CONSTRAINT \`FK_3cda7daeb4ff2b0dbc4b90736ca\` FOREIGN KEY (\`userID\`) REFERENCES \`user\`(\`ID\`) ON DELETE CASCADE ON UPDATE CASCADE`);
         await queryRunner.query(`ALTER TABLE \`user_maps_ranked_beatmapset\` ADD CONSTRAINT \`FK_f0b456f4502aebcbfd9233edf70\` FOREIGN KEY (\`beatmapsetID\`) REFERENCES \`beatmapset\`(\`ID\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+
+        if (!config.bn.username || !config.bn.secret) {
+            console.warn("WARNING: No BN website username/secret provided in config. Beatmapsets rankers will not be retrieved.");
+        } else {
+            let currentBeatmapsetId: number | null = null;
+            const bnsRawData = new Map<Beatmapset['ID'], Promise<null | BNEvent[]>>();
+
+            while (true) {
+                const where: FindCondition<Beatmapset> = { approvedDate: MoreThanOrEqual('2019-01-01T00:00:00.000Z') };
+                if (currentBeatmapsetId) {
+                    where.ID = MoreThan(currentBeatmapsetId);
+                }
+                const sets: Beatmapset[] = (await Beatmapset.find({
+                    where,
+                    order: { ID: 'ASC' },
+                    take: 100,
+                })).filter(set => set.approvedDate.getUTCFullYear() < 2021);
+
+                if (sets.length === 0)
+                    break;
+
+                sets.forEach(set => bnsRawData.set(set.ID, getBNsApiCallRawData(set.ID)));
+
+                for(const set of sets) {
+                    currentBeatmapsetId = set.ID;
+                    const rawData = await bnsRawData.get(set.ID);
+                    bnsRawData.delete(set.ID);
+                    if (rawData && rawData.length > 0) {
+                        set.rankers = await getRankers(rawData);
+                        console.log(`Adding ${set.rankers.length} rankers to set ${set.ID}`);
+                        await set.save();
+                    } else {
+                        console.log(`Adding no ranker to set ${set.ID}`);
+                    }
+                }
+            }
+        }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
