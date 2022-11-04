68c8165fd4295304fb7f8fd1718a7e4f1e7299d5
VINXIS <oykxf2@gmail.com>
2022-11-03 20:42:25 -0600

    consider apostrophe usage outside of posessive for gds

--- MCA/api/routes/nominating.ts
+++ MCA/api/routes/nominating.ts
@@ -231,7 +231,7 @@ nominatingRouter.post("/:year?/create", validatePhaseYear, isPhase("nomination")
                 });
                 const years: number[] = [];
                 for (const set of sets) 
-                    if (set.beatmaps.some(b => b.mode.ID === category.mode.ID && !b.difficulty.includes("'")))
+                    if (set.beatmaps.some(b => b.mode.ID === category.mode.ID && !(b.difficulty.includes("s'") || b.difficulty.includes("'s"))))
                         years.push(set.approvedDate.getUTCFullYear());
                 
                 if (Math.min(...years) !== ctx.state.year)
