38ab34c58ead28ef54f30b47d9752c365a5c863f
VINXIS <oykxf2@gmail.com>
2022-11-07 18:50:45 -0700

    Extra fixes to site stuff (#158)
    * grant endpoint for staff to give eligibility perms without gd
    
    * show beatmap names in vote order view
    
    * minor css touchup
    
    * also delete this
    
    * Only I Can Use This
    
    * restrictive regex

--- MCA/api/routes/staff/index.ts
+++ MCA/api/routes/staff/index.ts
@@ -1,8 +1,12 @@
 import Router from "@koa/router";
-import { isLoggedInDiscord, isStaff } from "../../../../Server/middleware";
+import { User as APIUser } from "nodesu";
+import { isCorsace, isLoggedInDiscord, isStaff } from "../../../../Server/middleware";
 import { MCA } from "../../../../Models/MCA_AYIM/mca";
 import { Category } from "../../../../Models/MCA_AYIM/category";
 import { validatePhaseYear } from "../../../../MCA-AYIM/api/middleware";
+import { OAuth, User } from "../../../../Models/user";
+import { MCAEligibility } from "../../../../Models/MCA_AYIM/mcaEligibility";
+import { osuClient } from "../../../../Server/osu";
 
 const staffRouter = new Router;
 
@@ -29,5 +33,52 @@ staffRouter.get("/categories/:year", validatePhaseYear, async (ctx) => {
 
     ctx.body = categories.map(x => x.getInfo());
 });
+
+// Endpoint for granting direct MCA nom/vote access to users
+staffRouter.post("/grant/:year", isCorsace, validatePhaseYear, async (ctx) => {
+    if (!ctx.request.body.user)
+        return ctx.body = { error: "No user ID given!" };
+    if (!ctx.request.body.mode)
+        return ctx.body = { error: "No mode given!" };
+    if (/^(standard|taiko|fruits|mania|storyboard)$/.test(ctx.request.body.mode))
+        return ctx.body = { error: "Invalid mode given!" };
+
+    const mca: MCA = ctx.state.mca;
+    let user = await User.findOne({
+        osu: {
+            userID: ctx.request.body.user,
+        },
+    });
+    if (!user) {
+        const apiUser = (await osuClient.user.get(ctx.request.body.user)) as APIUser;
+        if (!apiUser)
+            return ctx.body = { error: "Invalid user ID given!" };
+
+        user = new User;
+        user.country = apiUser.country.toString();
+        user.osu = new OAuth;
+        user.osu.userID = `${apiUser.userId}`;
+        user.osu.username = apiUser.username;
+        user.osu.avatar = "https://a.ppy.sh/" + apiUser.userId;
+        await user.save(); 
+    }
+    let eligibility = await MCAEligibility.findOne({
+        where: {
+            year: mca.year,
+            user,
+        },
+        relations: ["user"],
+    });
+
+    if (!eligibility) {
+        eligibility = new MCAEligibility();
+        eligibility.user = user;
+        eligibility.year = mca.year;
+    }
+
+    eligibility[ctx.request.body.mode] = true;
+    eligibility.storyboard = true;
+    await eligibility.save();
+});    
 
 export default staffRouter;
