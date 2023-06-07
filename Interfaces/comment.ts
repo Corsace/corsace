22318a4302fce709dacd3b713edaf47d319c6ded
VINXIS <oykxf2@gmail.com>
2023-06-07 17:48:26 -0600

    fix build

--- Interfaces/comment.ts
+++ Interfaces/comment.ts
@@ -1,7 +1,7 @@
 import { ModeDivision } from "./modes";
 import { User } from "./user";
 
-const profanityFilterString = "n(i|1)g|f(4|a)?gg?(0|o)?t|f(4|a)g|r(3|e)t(4|a)rd|c(0|o)(0|o)n|p(3|e)d(0|o)|p(3|e)n(1|i)s|c(0|o)ck|cum|v(4|a)g)|c(4|a)nc(e|3)r|kys"; 
+const profanityFilterString = "n(i|1)g|f(4|a)?gg?(0|o)?t|f(4|a)g|r(3|e)t(4|a)rd|c(0|o)(0|o)n|p(3|e)d(0|o)|p(3|e)n(1|i)s|c(0|o)ck|cum|v(4|a)g|c(4|a)nc(e|3)r|kys"; 
 
 export const profanityFilter = new RegExp(`${profanityFilterString}`, "gi");
 
