497edb7084df71f4b06e6d2ed623ea6ed57a38d6
VINXIS <oykxf2@gmail.com>
2023-07-15 11:44:19 -0600

    make momokai and corsace on front page clickable

--- Open/pages/index.vue
+++ Open/pages/index.vue
@@ -74,8 +74,18 @@
                         {{ $t('open.home.presentedBy') }}
                         <hr class="line--red line--no-space">
                         <div class="index_portal__image index_portal__image--row">
-                            <img src="../../Assets/img/partners/momokai.png">
-                            <img src="../../Assets/img/corsace-full.png">
+                            <a 
+                                href="http://momokai.com/corsace" 
+                                target="_blank"
+                            >
+                                <img src="../../Assets/img/partners/momokai.png">
+                            </a>
+                            <a 
+                                href="http://corsace.io"
+                                target="_blank"
+                            >
+                                <img src="../../Assets/img/corsace-full.png">
+                            </a>
                         </div>
                     </div>                
                 </div>
