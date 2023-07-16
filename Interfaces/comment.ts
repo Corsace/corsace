a78a8838a3be210a7d72619ed4321357425bd777
VINXIS <oykxf2@gmail.com>
2023-07-16 14:57:30 -0600

    sex filter

--- Interfaces/comment.ts
+++ Interfaces/comment.ts
@@ -1,11 +1,11 @@
 import { ModeDivision } from "./modes";
 import { User } from "./user";
 
-const profanityFilterString = "n(i|1)g|f(4|a)?gg?(0|o)?t|f(4|a)g|r(3|e)t(4|a)rd|c(0|o)(0|o)n|p(3|e)d(0|o)|p(3|e)n(1|i)s|c(0|o)ck|cum|v(4|a)g|c(4|a)nc(e|3)r|kys"; 
+const profanityFilterString = "n(i|1)g|f(4|a)?gg?(0|o)?t|f(4|a)g|r(3|e)t(4|a)rd|c(0|o)(0|o)n|p(3|e)d(0|o)|p(3|e)n(1|i)s|c(0|o)ck|cum|v(4|a)g|c(4|a)nc(e|3)r|kys; 
 
 export const profanityFilter = new RegExp(`${profanityFilterString}`, "gi");
 
-export const profanityFilterStrong = new RegExp(`${profanityFilterString}|fuck|sh(i|1)t|p(i|1)ss|(4|a)ss`, "gi");
+export const profanityFilterStrong = new RegExp(`${profanityFilterString}|fuck|sh(i|1)t|p(i|1)ss|(4|a)ss|s(e|3)x"`, "gi");
 
 export interface Comment {
     ID: number;
