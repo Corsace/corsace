76442b3f7b166b7e9a63b5d365ea986ff68acc29
Jeffrey Huang <jeffreyh.huang@mail.utoronto.ca>
2023-07-24 19:26:31 -0400

    Added SEO to all pages Changed Theme Icons

--- Open/pages/index.vue
+++ Open/pages/index.vue
@@ -109,9 +109,51 @@ const openModule = namespace("open");
     components: {
         OpenButton,
     },
+    // head () {
+    // const i18nHead = this.$nuxtI18nHead({ addSeoAttributes: true });
+    // return {
+    //     title: this.$store.state["open"].title,
+    //     meta: [
+    //         {hid: "description", name: "description", content: this.$store.state["open"].tournament.description},
+    //         {hid: "og:site_name", property: "og:site_name", content: this.$store.state["open"].title},
+    //         {hid: "og:title", property: "og:title", content: this.$store.state["open"].title},
+    //         {hid: "og:url", property: "og:url", content: this.$route.path}, 
+    //         {hid: "og:description", property: "og:description", content: `${this.$store.state["open"].tournament.description}`},
+    //         {hid: "og:image",property: "og:image", content: require("../../Assets/img/site/open/banner.png")},
+
+    //         {name: "twitter:title", content: this.$store.state["open"].title},
+    //         {name: "twitter:description", content: this.$store.state["open"].tournament.description},
+    //         {name: "twitter:image", content: require("../../Assets/img/site/open/banner.png")},
+    //         {name: "twitter:image:src", content: require("../../Assets/img/site/open/banner.png")},
+    //         // ...i18nHead.meta,
+    //     ],
+    //     link: [
+    //         {
+    //             rel: "canonical", hid: "canonical", href: `https://open.corsace.io`},
+    //         // ...i18nHead.link,
+    //     ],
+    // };
+    // },
     head () {
         return {
-            title: "Corsace Open",
+            title: this.$store.state["open"].title,
+            meta: [
+                {hid: "description", name: "description", content: this.$store.state["open"].tournament.description},
+
+                {hid: "og:site_name", property: "og:site_name", content: this.$store.state["open"].title},
+                {hid: "og:title", property: "og:title", content: this.$store.state["open"].title},
+                {hid: "og:url", property: "og:url", content: this.$route.path}, 
+                {hid: "og:description", property: "og:description", content: `${this.$store.state["open"].tournament.description}`},
+                {hid: "og:image",property: "og:image", content: require("../../Assets/img/site/open/banner.png")},
+                
+                {name: "twitter:title", content: this.$store.state["open"].title},
+                {name: "twitter:description", content: this.$store.state["open"].tournament.description},
+                {name: "twitter:image", content: require("../../Assets/img/site/open/banner.png")},
+                {name: "twitter:image:src", content: require("../../Assets/img/site/open/banner.png")},
+                
+                
+            ],
+            link: [{rel: "canonical", hid: "canonical", href: `https://open.corsace.io`}],
         };
     },
 })
--- Open/pages/qualifiers.vue
+++ Open/pages/qualifiers.vue
@@ -168,7 +168,24 @@ const openModule = namespace("open");
     },
     head () {
         return {
-            title: "Corsace Open",
+            title: this.$store.state["open"].title,
+            meta: [
+                {hid: "description", name: "description", content: this.$store.state["open"].tournament.description},
+
+                {hid: "og:site_name", property: "og:site_name", content: this.$store.state["open"].title},
+                {hid: "og:title", property: "og:title", content: this.$store.state["open"].title},
+                {hid: "og:url", property: "og:url", content: this.$route.path}, 
+                {hid: "og:description", property: "og:description", content: this.$store.state["open"].tournament.description},
+                {hid: "og:image",property: "og:image", content: require("../../Assets/img/site/open/banner.png")},
+                
+                {name: "twitter:title", content: this.$store.state["open"].title},
+                {name: "twitter:description", content: this.$store.state["open"].tournament.description},
+                {name: "twitter:image", content: require("../../Assets/img/site/open/banner.png")},
+                {name: "twitter:image:src", content: require("../../Assets/img/site/open/banner.png")},
+                
+                
+            ],
+            link: [{rel: "canonical", hid: "canonical", href: `https://open.corsace.io${this.$route.path}`}],
         };
     },
 })
