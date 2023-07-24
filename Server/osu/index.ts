fa6f93fc9dd0e89e75248390b8921fbcdf73d1dd
VINXIS <oykxf2@gmail.com>
2023-07-24 10:15:04 -0600

    remove api/v2 from base url

--- Server/osu/index.ts
+++ Server/osu/index.ts
@@ -6,6 +6,6 @@ import { osuAPIV2 } from "./osuAPIV2";
 const osuClient = new Client(config.osu.v1.apiKey, { parseData: true, baseUrl: `${config.osu.proxyBaseUrl || "https://osu.ppy.sh"}/api` });
 
 // API v2
-const osuV2Client = new osuAPIV2(config.osu.v2.clientId, config.osu.v2.clientSecret, { baseURL: `${config.osu.proxyBaseUrl || "https://osu.ppy.sh"}/api/v2` });
+const osuV2Client = new osuAPIV2(config.osu.v2.clientId, config.osu.v2.clientSecret, { baseURL: config.osu.proxyBaseUrl || "https://osu.ppy.sh" });
 
 export { osuClient, osuV2Client };
