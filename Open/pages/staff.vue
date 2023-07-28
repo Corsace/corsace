1f58b5bf55030c5d6ff45ca421551d4cc393e43e
Jeffrey Huang <48161673+JeffreyHuangToronto@users.noreply.github.com>
2023-07-28 16:25:55 -0400

    Open staff page (#263)
    * Component Created
    
    * Correct Design done
    
    * Placeholder Kaetwo
    
    * Removed spaces
    
    * Remove Kaetwo pic
    
    * Replaced kaetwo pic for corscae logo
    
    ---------
    
    Co-authored-by: VINXIS <oykxf2@gmail.com>
    Co-authored-by: Hugo Denizart <thepoon@osufr.live>

---
+++ Open/pages/staff.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="staff">
+        <div class="staff_main_content">
+            <OpenTitle>
+                STAFF
+            </OpenTitle>
+            <div class="staff_main_content_staff_list">
+                <StaffRow>
+                    <template #role>
+                        ORGANIZER
+                    </template>
+                </StaffRow>
+                <StaffRow>
+                    <template #role>
+                        DESIGN TEAM
+                    </template>
+                </StaffRow>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import OpenTitle from "../../Assets/components/open/OpenTitle.vue";
+import StaffRow from "../../Assets/components/open/StaffRow.vue";
+
+@Component({
+    components: {
+        OpenTitle,
+        StaffRow,
+    },
+    head () {
+        return {
+            title: this.$store.state["open"].title,
+            meta: [
+                {hid: "description", name: "description", content: this.$store.state["open"].tournament.description},
+
+                {hid: "og:site_name", property: "og:site_name", content: this.$store.state["open"].title},
+                {hid: "og:title", property: "og:title", content: this.$store.state["open"].title},
+                {hid: "og:url", property: "og:url", content: `https://open.corsace.io${this.$route.path}`}, 
+                {hid: "og:description", property: "og:description", content: this.$store.state["open"].tournament.description},
+                {hid: "og:image",property: "og:image", content: require("../../Assets/img/site/open/banner.png")},
+                
+                {name: "twitter:title", content: this.$store.state["open"].title},
+                {name: "twitter:description", content: this.$store.state["open"].tournament.description},
+                {name: "twitter:image", content: require("../../Assets/img/site/open/banner.png")},
+                {name: "twitter:image:src", content: require("../../Assets/img/site/open/banner.png")},
+            ],
+            link: [{rel: "canonical", hid: "canonical", href: `https://open.corsace.io${this.$route.path}`}],
+        };
+    },
+})
+export default class Staff extends Vue {
+}
+</script>
+
+<style lang="scss">
+@import '@s-sass/_variables';
+
+.staff {
+    &_main_content {
+        align-self: center;
+        position: relative;
+        width: 65vw;
+        padding: 35px;
+        background: linear-gradient(180deg, #1B1B1B 0%, #333333 261.55%);
+
+        &_staff_list {
+            padding-top: 20px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            gap: 20px;
+        }
+    }
+}
+
+</style>
