958e035cfae58edb89ef41b7bd30b9fb4b12a40b
VINXIS <oykxf2@gmail.com>
2023-08-09 14:35:56 -0600

    better discord error handler

--- DiscordBot/functions/error.ts
+++ DiscordBot/functions/error.ts
@@ -1,18 +1,26 @@
 import {  Message, ChatInputCommandInteraction, DiscordAPIError } from "discord.js";
+import commandUser from "./commandUser";
 import respond from "./respond";
 
-export default function errorHandler (err: unknown, m?: Message | ChatInputCommandInteraction) {
+export default async function errorHandler (err: unknown, m?: Message | ChatInputCommandInteraction): Promise<boolean> {
     if (!err)
         return false;
 
     if (!m) {
         if (!(err instanceof DiscordAPIError))
             console.log(err);
-        return;
+        return false;
     }
 
     if (err instanceof DiscordAPIError) {
-        respond(m, `The command was unable to be fulfilled.\nA discord error (code \`${err.code}\`) was received:\n\`\`\`\n${err.message}\n\`\`\``);
+        if (err.code === 50027) {
+            await m.channel?.send(`Ur command timed out Lol try again <@${commandUser(m).id}>`);
+            if (m instanceof Message && m.deletable)
+                await m.delete();
+            if (m instanceof ChatInputCommandInteraction)
+                await m.deleteReply();
+        } else
+            await respond(m, `The command was unable to be fulfilled.\nA discord error (code \`${err.code}\`) was received:\n\`\`\`\n${err.message}\n\`\`\``);
         return true;
     }
 
