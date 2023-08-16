38e7fb98fd96dc6ac120c941282a4136a38a4a18
VINXIS <oykxf2@gmail.com>
2023-08-16 10:38:36 -0600

    revert auto + socket split, add centrifugo publish qs

---
+++ BanchoBot/functions/tournaments/matchup/centrifugo.ts
@@ -0,0 +1,10 @@
+import Axios from "axios";
+import { config } from "node-config-ts";
+import { Matchup } from "../../../../Models/tournaments/matchup";
+
+export function publish (matchup: Matchup, data: any) {
+    return Axios.post(`${config.centrifugo.apiUrl}/publish`, {
+        channel: `matchup:${matchup.ID}`,
+        data,
+    });
+}
