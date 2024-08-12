b272dae1b87cd488e3063177bf3a22ec62dab690
VINXIS <oykxf2@gmail.com>
2024-08-11 21:47:33 -0600

    add trigger for 2 matchup connections already existing to ID

--- Models/migrations/1723429054291-matchupConnectionChanges.ts
+++ Models/migrations/1723429054291-matchupConnectionChanges.ts
@@ -8,9 +8,25 @@ export class MatchupConnectionChanges1723429054291 implements MigrationInterface
         await queryRunner.query(`ALTER TABLE \`matchup\` ADD \`winnerNextMatchupID\` int NULL`);
         await queryRunner.query(`ALTER TABLE \`matchup\` ADD CONSTRAINT \`FK_04b07f127ba49bcc5a1804d951e\` FOREIGN KEY (\`loserNextMatchupID\`) REFERENCES \`matchup\`(\`ID\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`matchup\` ADD CONSTRAINT \`FK_1153441e3dcc4503cb04115af00\` FOREIGN KEY (\`winnerNextMatchupID\`) REFERENCES \`matchup\`(\`ID\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`
+            CREATE TRIGGER check_matchup_connections
+            BEFORE UPDATE ON matchup
+            FOR EACH ROW
+            BEGIN
+                DECLARE connectionCount INT;
+                SELECT COUNT(*) INTO connectionCount
+                FROM matchup
+                WHERE loserNextMatchupID.ID = NEW.ID
+                OR winnerNextMatchupID = NEW.ID;
+                IF connection_count >= 2 THEN
+                    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Matchup cannot be connected to more than 2 matchups';
+                END IF;
+            END
+        `);
     }
 
     public async down (queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TRIGGER IF EXISTS check_matchup_connections`);
         await queryRunner.query(`ALTER TABLE \`matchup\` DROP FOREIGN KEY \`FK_1153441e3dcc4503cb04115af00\``);
         await queryRunner.query(`ALTER TABLE \`matchup\` DROP FOREIGN KEY \`FK_04b07f127ba49bcc5a1804d951e\``);
         await queryRunner.query(`ALTER TABLE \`matchup\` DROP COLUMN \`winnerNextMatchupID\``);
