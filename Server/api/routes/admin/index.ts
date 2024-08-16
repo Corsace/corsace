fd555cdd13aecb27c25151b32534a8a5da4ba6ca
Dao Heng Liu <daohengliu@gmail.com>
2024-08-17 00:04:54 +0100

    remove cache.del and cache.reset calls

--- Server/api/routes/admin/index.ts
+++ Server/api/routes/admin/index.ts
@@ -1,17 +1,9 @@
 import { CorsaceRouter } from "../../../corsaceRouter";
-import { cache } from "../../../../Server/cache";
 import { isCorsace, isLoggedInDiscord } from "../../../../Server/middleware";
 
 const adminRouter  = new CorsaceRouter();
 
 adminRouter.$use(isLoggedInDiscord);
 adminRouter.$use(isCorsace);
-
-adminRouter.$get("/reset", (ctx) => {
-    cache.reset();
-    ctx.body = {
-        success: true,
-    };
-});
 
 export default adminRouter;
