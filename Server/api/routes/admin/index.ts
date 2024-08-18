f42665423527b0d2ea2ba5b9c6ba7ac2774f858e
Dao Heng Liu <daohengliu@gmail.com>
2024-08-18 17:00:20 +0100

    restore admin /reset endpoint

--- Server/api/routes/admin/index.ts
+++ Server/api/routes/admin/index.ts
@@ -1,9 +1,17 @@
 import { CorsaceRouter } from "../../../corsaceRouter";
+import { cache } from "../../../../Server/cache";
 import { isCorsace, isLoggedInDiscord } from "../../../../Server/middleware";
 
 const adminRouter  = new CorsaceRouter();
 
 adminRouter.$use(isLoggedInDiscord);
 adminRouter.$use(isCorsace);
+
+adminRouter.$get("/reset", (ctx) => {
+    cache.reset();
+    ctx.body = {
+        success: true,
+    };
+});
 
 export default adminRouter;
