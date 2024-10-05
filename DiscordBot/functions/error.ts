55977435441097171cc606e415b2ecef84092656
VINXIS <oykxf2@gmail.com>
2024-10-05 06:47:27 -0600

    add stack trace button

--- DiscordBot/functions/error.ts
+++ DiscordBot/functions/error.ts
@@ -1,8 +1,10 @@
-import {  Message, ChatInputCommandInteraction, DiscordAPIError, TextChannel } from "discord.js";
+import {  Message, ChatInputCommandInteraction, DiscordAPIError, TextChannel, ActionRowBuilder, ButtonBuilder, ButtonStyle, MessageComponentInteraction } from "discord.js";
 import commandUser from "./commandUser";
 import respond from "./respond";
 import { discordClient } from "../../Server/discord";
 import { config } from "node-config-ts";
+import { randomUUID } from "crypto";
+import { filter } from "./messageInteractionFunctions";
 
 export default async function errorHandler (err: unknown, m?: Message | ChatInputCommandInteraction): Promise<boolean> {
     if (!err || !(err instanceof Error))
@@ -28,8 +30,27 @@ export default async function errorHandler (err: unknown, m?: Message | ChatInpu
 
     console.error(err);
     const channel = discordClient.channels.cache.get(config.discord.coreChannel);
-    if (channel instanceof TextChannel)
-        await channel.send(`${new Date().toISOString()}\nAn error unrelated to discord occurred while executing a discord command\nMessage/Interaction: \`${m instanceof Message ? m.content : m.commandName}\`\n\`\`\`${err}\`\`\``);
+    if (channel instanceof TextChannel) {
+        const stackID = randomUUID();
+        const message = await channel.send({
+            content: `${new Date().toISOString()}\nAn error unrelated to discord occurred while executing a discord command\nMessage/Interaction: \`${m instanceof Message ? m.content : m.commandName}\`\n\`\`\`${err}\`\`\``,
+            components: [new ActionRowBuilder<ButtonBuilder>().addComponents(
+                new ButtonBuilder()
+                    .setCustomId(stackID)
+                    .setLabel("See Stack Trace")
+                    .setStyle(ButtonStyle.Primary)
+            )],
+        });
+
+        const collector = message.createMessageComponentCollector({ filter, time: 6000000 });
+        collector.on("collect", async (i: MessageComponentInteraction) => {	
+            if (i.customId === stackID)
+                await i.update({
+                    content: `${new Date().toISOString()}\nAn error unrelated to discord occurred while executing a discord command\nMessage/Interaction: \`${m instanceof Message ? m.content : m.commandName}\`\n\`\`\`${err.stack}\`\`\``,
+                    components: [],
+                });
+        });
+    }
     await respond(m, "The command was unable to be fulfilled.\nAn error unrelated to discord occurred while executing this command. Contact VINXIS");
     return true;
 }
